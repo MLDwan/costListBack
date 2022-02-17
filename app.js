01f70502d2acd843f9aaaf1cc0ed319ac135b968
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-17 11:55:52 +0300

    change type date

--- app.js
+++ app.js
@@ -10,20 +10,19 @@ app.use(express.json());
 const { Schema } = mongoose;
 const costsSchema = new Schema({
   place: String,
-  date: Date,
-  spent: Number,
+  date: String,
+  spent: Number
 });
 
 const Cost = mongoose.model("costs", costsSchema);
 
-const uri =
-  "mongodb+srv://user:user@cluster0.x2u0b.mongodb.net/costs?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user:user@cluster0.x2u0b.mongodb.net/costs?retryWrites=true&w=majority";
 mongoose.connect(uri, { useUnifiedTopology: true });
 
 app.get("/", (req, res) => {
   Cost.find().then((result) => {
-    res.send({body: result})
-  })
+    res.send({body: result});
+  });
 });
 
 app.post("/createCosts", (req, res) => {
