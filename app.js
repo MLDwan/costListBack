775a8b733e77d27b6b5e350f290bdc8b91a015db
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-21 16:40:13 +0300

    fixed the code style

--- app.js
+++ app.js
@@ -10,58 +10,59 @@ const { Schema } = mongoose;
 const costsSchema = new Schema({
   place: String,
   date: String,
-  spent: Number
+  spent: Number,
 });
 
 const Cost = mongoose.model("costs", costsSchema);
 
-const uri = "mongodb+srv://user:user@cluster0.x2u0b.mongodb.net/costs?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://user:user@cluster0.x2u0b.mongodb.net/costs?retryWrites=true&w=majority";
 mongoose.connect(uri, { useUnifiedTopology: true });
 
 app.get("/allCosts", (req, res) => {
   try {
     Cost.find().then((result) => {
-      res.send({data: result});
+      res.send({ data: result });
     });
   } catch (error) {
-    res.status(422).send('Error! Params not correct');
-  };
+    res.status(422).send("Error! Params not correct");
+  }
 });
 
 app.post("/createCosts", (req, res) => {
-    const cost = new Cost(req.body);
-    try {
-      cost.save().then(() => {
-        Cost.find().then((result) => {
-          res.send({data: result});
-        });
+  const cost = new Cost(req.body);
+  try {
+    cost.save().then(() => {
+      Cost.find().then((result) => {
+        res.send({ data: result });
       });
-    } catch (error) {
-      res.status(422).send('Error! Params not correct');
-    }
+    });
+  } catch (error) {
+    res.status(422).send("Error! Params not correct");
+  }
 });
 
 app.delete("/deleteCosts", (req, res) => {
   const id = req.query._id;
-  if(id) {
-    Cost.deleteOne({ _id: id}).then(() => {
+  if (id) {
+    Cost.deleteOne({ _id: id }).then(() => {
       Cost.find().then((result) => {
-        res.send({data: result});
+        res.send({ data: result });
       });
     });
-  } else res.status(422).send('Error! Params not correct');
+  } else res.status(422).send("Error! Params not correct");
 });
 
 app.patch("/changeCost", (req, res) => {
-  const {body} = req;
+  const { body } = req;
   const id = req.body._id;
-  if(body && id) {
-    Cost.updateOne({_id: id}, body).then(() => {
+  if (body && id) {
+    Cost.updateOne({ _id: id }, body).then(() => {
       Cost.find().then((result) => {
-        res.send( {data: result} );
+        res.send({ data: result });
       });
     });
-  } else res.status(422).send('Error! Params not correct');
+  } else res.status(422).send("Error! Params not correct");
 });
 
 app.listen(8000, () => {
